6915f2df0407cb3af505032c381c8f1ed20a5ec9
Daria <igromova81@gmail.com>
2024-03-25 18:52:09 +0300

    cities saved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,45 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.model.City;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<City> citiesDirectory = new ArrayList<>();
+        try (Scanner cityDirectoryScanner = new Scanner(new File("src/main/resources/Задача ВС Java Сбер.csv"))) {
+            while (cityDirectoryScanner.hasNextLine()) {
+                citiesDirectory.add(getCityFromLine(cityDirectoryScanner.nextLine()));
+            }
+        } catch (FileNotFoundException ex) {
+            throw new FileNotFoundException();
         }
     }
+
+    private static City getCityFromLine(String line) {
+        City city = new City();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(";");
+            while (rowScanner.hasNext()) {
+                city.setId(Integer.parseInt(rowScanner.next()));
+                city.setName(rowScanner.next());
+                city.setRegion(rowScanner.next());
+                city.setDistrict(rowScanner.next());
+                city.setPopulation(Integer.parseInt(rowScanner.next()));
+                if (!rowScanner.hasNext()) {
+                    city.setFoundation("");
+                    break;
+                } else {
+                    city.setFoundation(rowScanner.next());
+                }
+            }
+        }
+        System.out.println(city);
+        return city;
+    }
+
 }
